70f1df5033a9e247ff5702cb8cc1c19139ea86a8
dev-kitty-ilnazik <ilnazm1ngaleev@yandex.ru>
2025-11-04 18:15:36 +0300

    commit

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 ROOT_DIR = Path(__file__).parent.parent
-ENV_FILE = ROOT_DIR / "src" / ".env"
+ENV_FILE = ROOT_DIR / ".env"
 BABEL_CONFIG_FILE = ROOT_DIR / "babel.cfg"
 
 LOCALES_DIR = ROOT_DIR / "locales"
